169998557f4a127ec9b03ba64915fa773a20c2f2
antklim <antklim@gmail.com>
2017-10-17 23:00:34 +1100

    API extention WIP

--- integrator.js
+++ integrator.js
@@ -1,3 +1,33 @@
-exports.merge = (api, integration) => {
+'use strict'
 
+const clone = require('clone')
+const obp = require('object-path')
+
+exports.extend = (api, integrationPaths) => {
+  const extendedApi = clone(api, false)
+  const integrations = exports.reduceIntegrations(integrationPaths)
+
+  for (let [path, integration] of integrations) {
+    const fullPath = `paths.${path}`
+    if (!obp.has(extendedApi, fullPath)) continue
+    obp.push(extendedApi, fullPath, integration)
+  }
+
+  return extendedApi
+}
+
+
+/**
+ * The method reduces an integration object and returns an array of tuples
+ * (path, integrations).
+ *
+ * integration <Object> - integration data
+ *
+ * return <Array> - array of tuples (path, integration)
+ */
+exports.reduceIntegrations = paths => {
+  return Object.entries(paths).reduce((accumulator, [path, methods]) => {
+    const arr = Object.entries(methods).map(([method, integrations]) => [`${path}.${method}`, integrations])
+    return accumulator.concat(arr)
+  }, [])
 }
--- integrator_test.js
+++ integrator_test.js
@@ -1,4 +1,100 @@
 const test = require('ava')
+const integrator = require('./integrator')
 
-test.skip('test', t => {
+test.beforeEach(t => {
+  t.context.api = {
+    paths: {
+      '/path1': {
+        get: {
+          description: 'get path1',
+          parameters: [],
+          responses: {}
+        },
+        post: {
+          description: 'post path1',
+          parameters: [],
+          responses: {}
+        },
+        put: {
+          description: 'put path1',
+          parameters: [],
+          responses: {}
+        }
+      },
+      '/path2': {
+        get: {
+          description: 'get path2',
+          parameters: [],
+          responses: {}
+        },
+        delete: {
+          description: 'delete path2',
+          parameters: [],
+          responses: {}
+        }
+      }
+    }
+  }
+  t.context.integrations = {
+    '/path1': {
+      get: {integrationA: 'integrationABody', integrationB: 'integrationBBody'},
+      post: {integrationC: 'integrationCBody'}
+    },
+    '/path2': {
+      delete: {integrationD: 'integrationDBody'}
+    }
+  }
+})
+
+test('extend()', t => {
+  const expected = {
+    paths: {
+      '/path1': {
+        get: {
+          description: 'get path1',
+          parameters: [],
+          responses: {},
+          integrationA: 'integrationABody',
+          integrationB: 'integrationBBody'
+        },
+        post: {
+          description: 'post path1',
+          parameters: [],
+          responses: {},
+          integrationC: 'integrationCBody'
+        },
+        put: {
+          description: 'put path1',
+          parameters: [],
+          responses: {}
+        }
+      },
+      '/path2': {
+        get: {
+          description: 'get path2',
+          parameters: [],
+          responses: {}
+        },
+        delete: {
+          description: 'delete path2',
+          parameters: [],
+          responses: {},
+          integrationD: 'integrationDBody'
+        }
+      }
+    }
+  }
+  const actual = integrator.extend(t.context.api, t.context.integrations)
+  t.deepEqual(actual, expected)
+  t.true(actual != t.context.api, 'extend() should not mutate api object, but return a new extended object')
+})
+
+test('reduceIntegrations() should reduce object into tuples', t => {
+  const expected = [
+    ['/path1.get', {integrationA: 'integrationABody', integrationB: 'integrationBBody'}],
+    ['/path1.post', {integrationC: 'integrationCBody'}],
+    ['/path2.delete', {integrationD: 'integrationDBody'}]
+  ]
+
+  t.deepEqual(integrator.reduceIntegrations(t.context.integrations), expected)
 })
