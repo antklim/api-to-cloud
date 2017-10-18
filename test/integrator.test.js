d38366a5c7c0406158e78a85127565b72669d588
antklim <antklim@gmail.com>
2017-10-18 16:48:43 +1100

    API extention implemented/tested

--- test/integrator.test.js
+++ test/integrator.test.js
@@ -2,51 +2,11 @@ const test = require('ava')
 const integrator = require('../lib/integrator')
 
 test.beforeEach(t => {
-  t.context.api = {
-    paths: {
-      '/path1': {
-        get: {
-          description: 'get path1',
-          parameters: [],
-          responses: {}
-        },
-        post: {
-          description: 'post path1',
-          parameters: [],
-          responses: {}
-        },
-        put: {
-          description: 'put path1',
-          parameters: [],
-          responses: {}
-        }
-      },
-      '/path2': {
-        get: {
-          description: 'get path2',
-          parameters: [],
-          responses: {}
-        },
-        delete: {
-          description: 'delete path2',
-          parameters: [],
-          responses: {}
-        }
-      }
-    }
-  }
-  t.context.integrations = {
-    '/path1': {
-      get: {integrationA: 'integrationABody', integrationB: 'integrationBBody'},
-      post: {integrationC: 'integrationCBody'}
-    },
-    '/path2': {
-      delete: {integrationD: 'integrationDBody'}
-    }
-  }
+  t.context.api = require('./fixtures/test-api.json')
+  t.context.integrations = require('./fixtures/test-integration.json')
 })
 
-test.skip('extend()', t => {
+test('extend()', t => {
   const expected = {
     paths: {
       '/path1': {
@@ -89,7 +49,7 @@ test.skip('extend()', t => {
   t.true(actual != t.context.api, 'extend() should not mutate api object, but return a new extended object')
 })
 
-test.skip('reduceIntegrations() should reduce object into tuples', t => {
+test('reduceIntegrations() should reduce object into tuples', t => {
   const expected = [
     ['/path1.get', {integrationA: 'integrationABody', integrationB: 'integrationBBody'}],
     ['/path1.post', {integrationC: 'integrationCBody'}],
