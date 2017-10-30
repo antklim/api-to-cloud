741f77ee0c1fe122b73a1c7f1c807a79b32e5e61
antklim <antklim@gmail.com>
2017-10-30 23:41:12 +1100

    testdoubles added, file operations unit tested

--- test/encoder.test.js
+++ test/encoder.test.js
@@ -1,7 +1,10 @@
+const fs = require('fs')
 const test = require('ava')
+const td = require('testdouble')
 const yaml = require('js-yaml')
-const path = require('path')
 const encoder = require('../lib/encoder')
+
+test.afterEach(t => td.reset())
 
 test('for() should return encoder for yaml', t => {
   const encoders = ['Yaml', 'yaml', 'yml'].map(encoder.for)
@@ -20,20 +23,31 @@ test('for() should return null for unsupported format', t => {
 })
 
 test('save() should reject format not supported', async t => {
-  const err = await t.throws(encoder.save({}, path.join(__dirname, 'fixtures', 'test.txt'), 'txt'))
+  const err = await t.throws(encoder.save({}, 'test.txt', 'txt'))
   t.is(err.message, 'Unsupported format \'txt\'')
 })
 
-// TODO: use sinon for the following tests
-test.skip('save() should reject if file cannot be written', async t => {
-  const err = await t.throws(encoder.save({}, path.join(__dirname, 'fixtures', 'write-test.json'), 'json'))
+test('save() should reject if file cannot be written', async t => {
+  const writeFile = td.function()
+  td.when(writeFile(td.matchers.anything(), td.matchers.anything()))
+    .thenCallback(new Error('no such file or directory'))
+  td.replace(fs, 'writeFile', writeFile)
+  const err = await t.throws(encoder.save({}, 'write-test.json', 'json'))
   t.regex(err.message, /no such file or directory/)
 })
 
-test.skip('save() should write JSON file', async t => {
-  t.deepEqual(await t.throws(encoder.save({}, path.join(__dirname, 'fixtures', 'write-test.json'), 'json')))
+test('save() should write JSON file', async t => {
+  const writeFile = td.function()
+  td.when(writeFile(td.matchers.anything(), td.matchers.anything())).thenCallback()
+  td.replace(fs, 'writeFile', writeFile)
+  await t.notThrows(encoder.save({foo: 'bar'}, 'write-test.json', 'json'))
+  td.verify(writeFile('write-test.json', '{"foo":"bar"}', td.matchers.isA(Function)))
 })
 
-test.skip('save() should write YAML file', async t => {
-  t.deepEqual(await t.throws(encoder.save({}, path.join(__dirname, 'fixtures', 'write-test.yaml'), 'yaml')))
+test('save() should write YAML file', async t => {
+  const writeFile = td.function()
+  td.when(writeFile(td.matchers.anything(), td.matchers.anything())).thenCallback()
+  td.replace(fs, 'writeFile', writeFile)
+  await t.notThrows(encoder.save({foo: 'bar'}, 'write-test.yaml', 'yaml'))
+  td.verify(writeFile('write-test.yaml', 'foo: bar\n', td.matchers.isA(Function)))
 })
