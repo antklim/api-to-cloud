dd59928440f24387da49964b07621227450cbfda
antklim <antklim@gmail.com>
2017-11-04 19:02:27 +1100

    Clean API definition from unsupported features

--- test/integrator.test.js
+++ test/integrator.test.js
@@ -69,7 +69,64 @@ test('extend()', t => {
   /* eslint-enable eqeqeq */
 })
 
-test.todo('extend() should exclude fields unsupported by AWS')
+test('cloneClean() should exclude fields unsupported by AWS', t => {
+  const expected = {
+    paths: {
+      '/path1': {
+        get: {
+          description: 'get path1',
+          parameters: [],
+          responses: {},
+        },
+        post: {
+          description: 'post path1',
+          parameters: [],
+          responses: {},
+        },
+        put: {
+          description: 'put path1',
+          parameters: [],
+          responses: {},
+        },
+      },
+      '/path2': {
+        get: {
+          description: 'get path2',
+          parameters: [],
+          responses: {},
+        },
+        delete: {
+          description: 'delete path2',
+          parameters: [],
+          responses: {},
+        },
+      },
+    },
+    definitions: {
+      Pet: {
+        type: 'object',
+        required: ['id', 'name'],
+        properties: {
+          id: {type: 'integer', format: 'int64'},
+          name: {type: 'string'},
+          tag: {type: 'string'},
+        },
+      },
+      Error: {
+        required: ['code', 'message'],
+        properties: {
+          code: {type: 'integer', format: 'int32'},
+          message: {type: 'string'},
+        },
+      },
+    },
+  }
+  const actual = integrator.cloneClean(t.context.api, 'AWS')
+  t.deepEqual(actual, expected)
+  /* eslint-disable eqeqeq */
+  t.true(actual != t.context.api, 'extend() should not mutate api object, but return a new extended object')
+  /* eslint-enable eqeqeq */
+})
 
 test('reduceIntegrations() should reduce object into tuples', t => {
   const expected = [
